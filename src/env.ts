991c6fc04afd2432419c53014d7ea587d712317a
Václav Kubernát <sir.venceslas@gmail.com>
2021-01-11 03:52:12 +0100

    Allow specifying env vars

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ if (typeof process.env.OAUTH_TOKEN !== "string") {
 }
 
 export default {
-    BOT_USERNAME: "ucipina",
-    CHANNEL_NAME: "syyyr",
+    BOT_USERNAME: process.env.BOT_USERNAME || "ucipina",
+    CHANNEL_NAME: process.env.CHANNEL_NAME || "syyyr",
     OAUTH_TOKEN: process.env.OAUTH_TOKEN
 };
