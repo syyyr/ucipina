65eb6946c32bf89c4bb4cb68eb5fb124009e7fb5
Václav Kubernát <sir.venceslas@gmail.com>
2021-01-11 03:52:58 +0100

    Add !bot-uptime

--- src/excl_commands.ts
+++ src/excl_commands.ts
@@ -4,15 +4,32 @@ import MessageQueue from "./msg_queue";
 import {log} from "./log"
 import wrapHandler from "./wrap_handler";
 
+const startTime = Date.now();
+
+const secondsInDay = 3600 * 24;
+const secondsInHour = 3600;
+const secondsInMinute = 60;
+
 const exclCommandHandlers: {[key in string]: (msgQueue: MessageQueue) => void} = {
     "!commands": (msgQueue: MessageQueue) => {
         msgQueue.push(`Příkazy: ${Object.keys(exclCommandHandlers).sort().join(" ")}`);
     },
     "!bot": (msgQueue: MessageQueue) => {
-        msgQueue.push(`AHOJ! Já jsem ${ENV.BOT_USERNAME} a jsem bot na tomto kanálu. :^)`);
+        msgQueue.push(`AHOJ! Já jsem ${ENV.BOT_USERNAME} a jsem bot na kanálu ${ENV.CHANNEL_NAME}. :^)`);
     },
     "!youtube": (msgQueue: MessageQueue) => {
         msgQueue.push("Bot podporuje YouTube linky. Stačí je poslat do chatu (klidně i víc najednou.");
+    },
+    "!bot-uptime": (msgQueue: MessageQueue) => {
+        let upTimeSec = Math.floor((Date.now() - startTime) / 1000);
+        const days = Math.floor(upTimeSec / secondsInDay);
+        upTimeSec = upTimeSec - (days * secondsInDay);
+        const hours = Math.floor(upTimeSec / secondsInHour);
+        upTimeSec = upTimeSec - (hours * secondsInHour);
+        const minutes = Math.floor(upTimeSec / secondsInMinute);
+        upTimeSec = upTimeSec - (minutes * secondsInMinute);
+
+        msgQueue.push(`Už běžím ${days}d${hours}h${minutes}m${upTimeSec}s.`);
     }
 }
 
